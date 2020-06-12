e9715071cc189f527650d4e884967fdd4799a375
osman-sahin <osmansahin.eng@gmail.com>
2020-06-12 18:40:43 +0300

    Entities

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public abstract class BaseEntity   // abstract new'lenemez. Db'de oluşturulmaz
+    {
+        public virtual int Id { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/Config/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Config
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(b => b.CategoryName)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
